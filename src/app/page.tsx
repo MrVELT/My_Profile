89f2e3d295995120a4be9d4d1e0f1ebf72e3594a
VELT <nguyenminhduc7899@gmail.com>
2025-01-15 23:52:18 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
+'use client';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faUserTie } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Avatar } from 'antd';
+import { motion } from 'motion/react';
+import { FaReact } from 'react-icons/fa';
+import { IoCodeSlashOutline } from 'react-icons/io5';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="bg-blue-600 flex items-center justify-center py-20">
+        <motion.div
+          className="flex flex-col gap-5 items-center"
+          initial={{ y: -100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          //viewport={{ once: true, amount: 0.3 }}
+          transition={{
+            duration: 0.8,
+            ease: 'easeOut',
+          }}
+        >
+          <Avatar size={150} icon={<FontAwesomeIcon icon={faUserTie} />} />
+          <p className="font-bold text-3xl text-white">Frontend Developer</p>
+          <p className="text-2xl text-white">Chuyên gia Next.js</p>
+          <button className="flex items-center justify-center px-5 py-2 gap-2 rounded-2xl bg-white text-blue-500 hover:bg-gray-100">
+            <FontAwesomeIcon icon={faGithub} />
+            <p>Github</p>
+          </button>
+        </motion.div>
+      </div>
+      <div className="flex flex-col items-center justify-center py-20 bg-white gap-10">
+        <h1 className="text-3xl font-bold text-black">Kỹ năng chuyên môn</h1>
+        <div className="grid md:flex gap-10">
+          <motion.div
+            className="grid p-5 gap-3 rounded-lg bg-gray-50"
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            //viewport={{ once: true, amount: 0.3 }}
+            transition={{
+              duration: 0.8,
+              ease: 'easeOut',
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <FaReact size={32} className="text-blue-500" />
+            <p className="font-bold text-xl text-black">Frontend Development</p>
+            <p className="text-black">Next.js, TypeScript, Tailwind CSS</p>
+          </motion.div>
+          <motion.div
+            className="grid p-5 gap-3 rounded-lg bg-gray-50"
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            //viewport={{ once: true, amount: 0.3 }}
+            transition={{
+              duration: 0.8,
+              ease: 'easeOut',
+            }}
           >
-            Read our docs
-          </a>
+            <IoCodeSlashOutline size={32} className="text-blue-500" />
+            <p className="font-bold text-xl text-black">Clean Code</p>
+            <p className="text-black">Best Practices, Code Review</p>
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="py-20 gap-10 flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-black">Dự án tiêu biểu</h1>
+      </div>
+    </>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-800 flex items-center justify-center text-white h-[100px]">
+      @2025 Mr.VELT Frontend Developer Profile.
+    </div>
+  );
+};
+
+export default Footer;
